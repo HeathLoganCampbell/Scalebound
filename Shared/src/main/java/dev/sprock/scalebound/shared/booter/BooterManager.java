e868a3aabf6b329f8ebeb01feb07301ef91b9b15
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2020-07-08 18:19:23 +1200

    stop script

--- Shared/src/main/java/dev/sprock/scalebound/shared/booter/BooterManager.java
+++ Shared/src/main/java/dev/sprock/scalebound/shared/booter/BooterManager.java
@@ -25,26 +25,6 @@ public class BooterManager
         this.ssh = new JSch();
     }
 
-    public static void main(String[] args) {
-
-        String jenkinsUsername = "sprock";
-        String jenkinsToken = "11966b20dd60cc5e083e0e6fea81e3aaba";
-        StartUpScriptBuilder script = new StartUpScriptBuilder();
-
-        script.addLine("echo 'Rarrr'");
-        script.addLine("echo 'Downloading'");
-        script.addLine("pwd");
-        script.addLine("mkdir Plugins");
-        script.addLine("curl -u " + jenkinsUsername + ":" + jenkinsToken + " -o Plugins/Library.jar \"http://51.222.35.161:8080/job/DreamTestNetwork/job/\\$1/job/master/lastSuccessfulBuild/execution/node/3/ws/Library.jar\"");
-        script.addLine("pwd");
-        script.addLine("cd Plugins");
-        script.addLine("ls");
-
-        new BooterManager().executeCommand("ubuntu", "ns517441.ip-158-69-27.net", script.toString());
-
-    }
-    //--port %SERVER_PORT% --host 0.0.0.0 --max-players %SERVER_MAX_PLAYER_COUNT%
-
     private String injectPlaceholders(String command, MinecraftServer minecraftServer, ServerProfile profile)
     {
         return command
@@ -59,9 +39,6 @@ public class BooterManager
     }
 
     public void startServer(MinecraftServer minecraftServer, ProfileManager profileManager) {
-        //Download spigot/paper/bukkit/velocity/bungee
-        //Download Plugins
-        //Download Configs
         final ServerProfile profile = profileManager.getProfileById(minecraftServer.getProfileId());
 
         final ProfileContent content = profile.getContent();
@@ -71,78 +48,12 @@ public class BooterManager
         }
 
         executeCommand("ubuntu", minecraftServer.getAddress(), script.toString());
-//        final Session session = this.ssh.getSession(SSH_USERNAME, minecraftServer.getAddress(), 22);
-//        session.connect();
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//        ProcessRunner processRunner = new ProcessRunner(
-//                file,
-//                "/bin/sh",
-//                "remoteStart.sh",
-//                minecraftServer.getAddress(),
-//                minecraftServer.getPort() + "",
-//                minecraftServer.getMaxRamMB() + "",
-//                minecraftServer.getServerName(),
-//                profile.getName()
-//        );
-//
-//        processRunner.start((error) -> {
-//            if(error)
-//            {
-//                System.out.println("Error creating server");
-//            }
-//            else
-//            {
-//                System.out.println("server created");
-//            }
-//        });
-//
-//        try {
-//            processRunner.join(100L);
-//        } catch (InterruptedException e1) {
-//            e1.printStackTrace();
-//        }
-//
-//        if (!processRunner.isDone())
-//            System.out.println("Still going");;
     }
 
     public void stopServer(MinecraftServer minecraftServer)
     {
-        ProcessRunner processRunner = new ProcessRunner(
-                new File("./scripts"),
-                "/bin/sh",
-                "remoteKill.sh",
-                minecraftServer.getAddress(),
-                minecraftServer.getServerName()
-        );
-        processRunner.start((error) -> {
-            if(error)
-            {
-                System.out.println("Error killing server");
-            }
-            else
-            {
-                System.out.println("server killed");
-            }
-        });
 
-        try {
-            processRunner.join(50L);
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
-        }
-
-        if (!processRunner.isDone())
-            System.out.println("Still going");;
+        executeCommand("ubuntu", minecraftServer.getAddress(), "for session in $(screen -ls | grep -o '[0-9]*\\." + minecraftServer.getServerName() + "'); do screen -S \"${session}\" -X quit; done");
     }
 
     private void executeCommand(String user, String host, String command)
